ed3a66f9d23c128a85b7b2c0bf8a85c819b71755
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-11-14 22:40:13 +0000

    Change EOH to EOF in AUTHORS script generator

--- generate_authors.sh
+++ generate_authors.sh
@@ -2,13 +2,15 @@
 set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
+
 # see also ".mailmap" for how email addresses and names are deduplicated
 
 {
-	cat <<-'EOH'
-	# This file lists all individuals having contributed content to the repository.
+	cat <<-'EOF'
+	# This file lists all individuals having contributed content to the
+	  repository.
 	# For how it is generated, see `generate-authors.sh`.
-	EOH
+	EOF
 	echo
 	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
 } > AUTHORS
