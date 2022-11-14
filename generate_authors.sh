b73397413c443e7006ef98efbc1fab95abc92072
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-11-14 22:02:53 +0000

    Remove space in generate_authors.sh

--- generate_authors.sh
+++ generate_authors.sh
@@ -2,13 +2,11 @@
 set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
-
 # see also ".mailmap" for how email addresses and names are deduplicated
 
 {
-	cat << EOH
-	# This file lists all individuals having contributed content to
-	  the repository.
+	cat <<-'EOH'
+	# This file lists all individuals having contributed content to the repository.
 	# For how it is generated, see `generate-authors.sh`.
 	EOH
 	echo
